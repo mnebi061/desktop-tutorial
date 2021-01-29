7b9fbef4b5e485f659b8d0b85dcb867f0982965f
mnebi061 <73642582+mnebi061@users.noreply.github.com>
2021-01-29 14:27:37 +0300

    Add files via upload

---
+++ tüm kalıntılar.js
@@ -0,0 +1,107 @@
+// Every web browsers has a developer tool, for example Google Chrome, right click and then click "inspect element" then click -> console tab
+undefined
+// "window" command has own functions let's apply this in console window
+undefined
+window // and click enter for apply
+Window {0: global, window: Window, self: Window, document: document, name: "", location: Location, …}
+// We can extend and see all the methods in it
+undefined
+"These are my words" // we can define string like that
+"These are my words"
+"If i want to say \"Hello\" i have to use backslash" // backslash is used to indicate speech in a sentence
+"If i want to say "Hello" i have to use backslash"
+'I said "hello"' // we can create like that
+"I said "hello""
+"You said 'hi'" // and also we can create like that
+"You said 'hi'"
+// We have 2 number data type, 1st is integer and there is no fractional part, and 2nd is float like 2.3 4.6 let me illustrate
+undefined
+4 // this is integer
+4
+5.6 //this is float
+5.6
+// Also we have boolean expression like other programming languages, zeros and ones or true and false
+undefined
+true
+true
+false
+false
+//There is null expression, the expression has nothing to return, void
+undefined
+//There is undefined expression, its undefined or doesnt have anything assigned
+undefined
+// NaN primitive type, its not a number
+undefined
+//When we create a variable we use var keyword and then we write variable name its means address actually, after we use "=" equal mark, declare a variable inside it. At the end of the declaration we use semicolon. let me illustrate this
+undefined
+var name = "Nebi";  // this could be a string a number a boolean
+undefined
+// lets control it
+undefined
+name
+"Nebi"
+// We can change the value inside the variable we created as name after
+undefined
+name = "Muhammed"; // we use semicolon for end the sentence
+"Muhammed"
+name
+"Muhammed"
+// we dont have to write a value for variable, we can create a null variable
+undefined
+var fname;
+undefined
+// We can assign a value later
+undefined
+fname = "Nebi";
+"Nebi"
+fname
+"Nebi"
+// This is a loosely data type language, we can create a variable for a string but we can change it immediately after assign
+undefined
+fname = "string";
+"string"
+fname = 200;
+200
+// As we can see we assigned fname as a string but we could change it to integer
+undefined
+// We can create constant variables for cant change after
+undefined
+const sname = "Boler";
+undefined
+// We can not change sname now lets try
+undefined
+sname = "bllr";
+VM5987:1 Uncaught TypeError: Assignment to constant variable.
+    at <anonymous>:1:7
+// As we can see we cant change constant variables after declared
+undefined
+// We can see the variables we created inside window command
+undefined
+window
+Window {0: global, window: Window, self: Window, document: document, name: "Muhammed", location: Location, …}
+// We have operators and their precedence, we have B0DMAS for encryption. B is paranthesis and has a higher precedence from the others. Then it comes power as named as 0 in password. And then division and multiplication has higher precedence. The las is addition and substraction.
+undefined
+10 + 5 * 3
+25
+Math.pow(4,2) / 2 + 5 - 3 + (2 * 3)  //math.power is a function for using power of number
+16
+9 % 5// There is a mode operator "%", we can take mode with this
+4
+// We can use this operation in assignment like this
+undefined
+var nombre = 15;
+undefined
+nombre +=5; // 5 added at nombre variable
+20
+// nombre is equal to 20 anymore
+undefined
+"Muhammed" + " Nebi"; //We can add a word to word with addition
+"Muhammed Nebi"
+"Nebi " + 61; // We can do it too;
+"Nebi 61"
+// + operator is a function
+undefined
+name += fname ;
+"Muhammed200"
+// we can use this like this too
+undefined
